2e04a366b1e6e5e09a9bc9d2feebb0deca17994e
Pinokinoxoxo <102460408+Pinokinoxoxo@users.noreply.github.com>
2023-11-11 19:09:34 +0900

    Create ImageClassifierHelper.kt

---
+++ ImageClassifierHelper.kt
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.lite.examples.imageclassification
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import android.util.Log
+import android.view.Surface
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.Rot90Op
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+
+class ImageClassifierHelper(
+    var threshold: Float = 0.5f,
+    var numThreads: Int = 1,
+    var maxResults: Int = 1,
+    var currentDelegate: Int = 0,
+    var currentModel: Int = 0,
+    val context: Context,
+    val imageClassifierListener: ClassifierListener?
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+    fun clearImageClassifier() {
+        imageClassifier = null
+    }
+
+    private fun setupImageClassifier() {
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
+
+        when (currentDelegate) {
+            DELEGATE_CPU -> {
+                // Default
+            }
+            DELEGATE_GPU -> {
+                if (CompatibilityList().isDelegateSupportedOnThisDevice) {
+                    baseOptionsBuilder.useGpu()
+                } else {
+                    imageClassifierListener?.onError("GPU is not supported on this device")
+                }
+            }
+            DELEGATE_NNAPI -> {
+                baseOptionsBuilder.useNnapi()
+            }
+        }
+
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        val modelName = "efficientnet_cifar10_37.3.tflite"
+
+        try {
+            imageClassifier =
+                ImageClassifier.createFromFileAndOptions(context, modelName, optionsBuilder.build())
+        } catch (e: IllegalStateException) {
+            imageClassifierListener?.onError(
+                "Image classifier failed to initialize. See error logs for details"
+            )
+            Log.e(TAG, "TFLite failed to load model with error: " + e.message)
+        }
+    }
+
+    fun classify(image: Bitmap, rotation: Int) {
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        var inferenceTime = SystemClock.uptimeMillis()
+
+        // 이미지 프로세서 설정
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(32, 32, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR)) // 크기 조정
+            .add(Rot90Op(rotation)) // 회전
+            .add(NormalizeOp(0f, 127.5f)) // 정규화
+            .build()
+
+        // 이미지 전처리 및 TensorImage로 변환
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
+
+        // 분류 실행
+        val results = imageClassifier?.classify(tensorImage)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        imageClassifierListener?.onResults(results, inferenceTime)
+    }
+
+    // Receive the device rotation (Surface.x values range from 0->3) and return EXIF orientation
+    // http://jpegclub.org/exif_orientation.html
+    private fun getOrientationFromRotation(rotation: Int) : ImageProcessingOptions.Orientation {
+        when (rotation) {
+            Surface.ROTATION_270 ->
+                return ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            Surface.ROTATION_180 ->
+                return ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            Surface.ROTATION_90 ->
+                return ImageProcessingOptions.Orientation.TOP_LEFT
+            else ->
+                return ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?,
+            inferenceTime: Long
+        )
+    }
+
+    companion object {
+        const val DELEGATE_CPU = 0
+        const val DELEGATE_GPU = 1
+        const val DELEGATE_NNAPI = 2
+        const val MODEL_MOBILENETV1 = 0
+        const val MODEL_EFFICIENTNETV0 = 1
+        const val MODEL_EFFICIENTNETV1 = 2
+        const val MODEL_EFFICIENTNETV2 = 3
+
+        private const val TAG = "ImageClassifierHelper"
+    }
+}
